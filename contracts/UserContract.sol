f7c13bd9e4d4aea7dc4b84447d73c4d3b8f62228
ari4325 <ari4325@live.com>
2022-11-02 14:21:02 +0530

    added functionality for product

--- contracts/UserContract.sol
+++ contracts/UserContract.sol
@@ -4,17 +4,18 @@ pragma solidity ^ 0.8.4;
 
 import "./geoBlockchain.sol";
 
-contract User {
+contract UserContract {
   uint public userId;
   string public userName;
   string public Address;
   string public contactNumber;
   string public location;
   string public AadharId;         // basic user information
+  string public product_id;
 
-  geoBlockchain parentContract;
+  address parentContract;
 
-  constructor(geoBlockchain _parentContract, uint _userId, string memory _userName, string memory _Address, string memory _contactNumber, string memory _location, string memory _AadharId) {
+  constructor(address _parentContract, uint _userId, string memory _userName, string memory _Address, string memory _contactNumber, string memory _location, string memory _AadharId) {
     parentContract = _parentContract;
     userId = _userId;
     userName = _userName;
@@ -22,5 +23,12 @@ contract User {
     contactNumber = _contactNumber;
     location = _location;
     AadharId = _AadharId;
+  }
+
+  function addProduct(string memory _id) external returns (bool){
+    require(msg.sender == parentContract, "can update only through delegate calls");
+    product_id =  _id;
+
+    return true;
   }
 }
